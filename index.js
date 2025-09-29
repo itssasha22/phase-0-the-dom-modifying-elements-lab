d43770ee4ca4ed57bd72f7dabfbc039c09d097b5
sasha lisa <lisasashaa@gmail.com>
2025-09-29 20:22:23 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+
+// Remove the main element
+document.querySelector('main#main').remove();
+
+// Create new header
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.innerHTML = 'Sasha is the champion';
+
+// Append to body
+document.body.append(newHeader);
